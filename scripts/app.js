30e35d8e33d2734d32de0fd7dead970cab1845a9
theoverse <theoverse@protonmail.com>
2022-02-09 15:23:15 -0800

    added update (change) chatroom feature

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list')
 const newChatForm = document.querySelector('.new-chat')
 const newNameForm = document.querySelector('.new-name')
 const updateMsg = document.querySelector('.update-msg')
+const rooms = document.querySelector('.chat-rooms')
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -26,6 +27,16 @@ newNameForm.addEventListener('submit', e => {
     // show then hide the update message
     updateMsg.innerText = `Your name was updated to ${newName}`
     setTimeout(() => updateMsg.innerText = '', 3000)
+})
+
+// update the chat room
+rooms.addEventListener('click', e => {
+    // console.log(e)
+    if (e.target.nodeName === 'BUTTON') {
+        chatUI.clear()
+        chatroom.updateRoom(e.target.getAttribute('id'))
+        chatroom.getChats(chat => chatUI.render(chat))
+    }
 })
 
 // check local storage for a name
