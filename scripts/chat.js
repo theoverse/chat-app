4c1dee579312b366cef8b922d797393f3068ee7e
theoverse <theoverse@protonmail.com>
2022-02-09 14:16:18 -0800

    added chatroom and username update methods

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
         this.room = room
         this.username = username
         this.chats = db.collection('chats')
+        this.unsub
     }
     async addChat(message) {
         // format a chat object
@@ -23,7 +24,7 @@ class Chatroom {
         return response
     }
     getChats(callback) {
-        this.chats
+        this.unsub = this.chats
         .where('room', '==', this.room)
         .orderBy('created_at')
         .onSnapshot(snapshot => {
@@ -34,6 +35,16 @@ class Chatroom {
                 }
             })
         })
+    }
+    updateName(username) {
+        this.username = username
+    }
+    updateRoom(room) {
+        this.room = room
+        console.log('room updated')
+        if (this.unsub) {
+            this.unsub()
+        }
     }
 }
 
@@ -47,3 +58,12 @@ const chatroom = new Chatroom('general', 'john')
 chatroom.getChats((data) => {
     console.log(data)
 })
+
+setTimeout(() => {
+    chatroom.updateRoom('gaming')
+    chatroom.updateName('marco')
+    chatroom.getChats((data) => {
+        console.log(data)
+    })
+    chatroom.addChat('hello')
+}, 3000)
